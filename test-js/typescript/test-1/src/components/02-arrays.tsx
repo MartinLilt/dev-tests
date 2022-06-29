a741adf9c8df98e6349257437ea357dc7a6a58fa
MartinLilt <80705619+MartinLilt@users.noreply.github.com>
2022-06-29 17:06:27 +0300

    #

---
+++ test-js/typescript/test-1/src/components/02-arrays.tsx
@@ -0,0 +1,9 @@
+const temps: readonly (number | string)[] = [1, 2, 3, 4, 5, 6, 'qwe'];
+const nembers: Array<number> = [1, 2, 3, 4, 5, 6];
+
+const coords: [number, number] = [12.43433, 32.32535];
+const rgb: [number, number, number] = [255, 0, 100];
+
+console.log(temps, nembers, coords, rgb);
+
+export {};
